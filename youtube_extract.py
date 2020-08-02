d9b5e7174852bfa9a5b003d86c492331f8fade77
“maki5588” <your-mail@example.com>
2020-08-03 01:57:02 +0900

    add get_page_source

--- youtube_extract.py
+++ youtube_extract.py
@@ -14,24 +14,15 @@ except ImportError:
     import urllib.parse as urlparse
 
 
-
-# data = pd.read_csv('sample.csv', sep=",")
-# print(data)
-# 予め列を挿入しておく必要がある。
-# data['new_column'] = ''
-
-# # 列に値を代入
-# for index, row in data.iterrows():
-#     data['new_column'][index] = 'hoge'
-
-# data.to_csv('sample.csv', index=False)
-
-# print(open('sample.csv').read())
 class ChannelCountryScraper(object):
+
+	def __init__(self):
+		self.channel_about_urls = []
+
 
 	def run(self):
 		self.read_youtube_urls()
-		# self.get_page_source()
+		self.get_page_source()
 		# self.channel_country_data_save_as_csv_file()
 
 
@@ -40,29 +31,35 @@ class ChannelCountryScraper(object):
 		channel_urls_ndarray = channel_url_data.index.values
 		channel_urls = channel_urls_ndarray.tolist()
 		for i in channel_urls:
-			self.youtube_url = 'https://www.youtube.com'
-			self.channel_url = ('%s' % i)
-			self.open_channel_url = urlparse.urljoin(self.youtube_url, self.channel_url, 'about')
+			youtube_url = 'https://www.youtube.com'
+			channel_url = ('%s' % i)
+			about_url = 'about'
+			channel_about_url = urlparse.urljoin(youtube_url, channel_url+'/about')
+			print(channel_about_url)
+			self.channel_about_urls.append(channel_about_url)
 
-	# def get_page_source(self):
-	# 	self.driver = webdriver.Chrome()
-	# 	self.driver.get(self.open_channel_url)
-	# 	self.current_html = self.driver.page_source
-	# 	element = self.driver.find_element_by_xpath('//*[@class="style-scope ytd-page-manager"]')
-	# 	actions = ActionChains(self.driver)
-	# 	actions.move_to_element(element)
-	# 	actions.perform()
-	# 	actions.reset_actions()
-	# 	while True:
-	# 		for j in range(100):
-	# 			actions.send_keys(Keys.PAGE_DOWN)
-	# 		actions.perform()
-	# 		sleep(2.5)
-	# 		html = self.driver.page_source
-	# 		if self.current_html != html:
-	# 			self.current_html=html
-	# 		else:
-	# 			break
+
+	def get_page_source(self):
+		self.driver = webdriver.Chrome()
+		for i in self.channel_about_urls:
+			self.driver.get(i)
+			self.current_html = self.driver.page_source
+			element = self.driver.find_element_by_xpath('//*[@class="style-scope ytd-page-manager"]')
+			actions = ActionChains(self.driver)
+			actions.move_to_element(element)
+			actions.perform()
+			actions.reset_actions()
+			while True:
+				for j in range(100):
+					actions.send_keys(Keys.PAGE_DOWN)
+				actions.perform()
+				sleep(2.5)
+				html = self.driver.page_source
+				if self.current_html != html:
+					self.current_html=html
+				else:
+					self.driver.close()
+					break
 
 
 if __name__ == "__main__":
