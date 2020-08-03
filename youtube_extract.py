06856c45c2ce892371181a52414e66e385540a98
“maki5588” <your-mail@example.com>
2020-08-03 19:43:39 +0900

    channel_subscriber_additional

--- youtube_extract.py
+++ youtube_extract.py
@@ -18,12 +18,14 @@ class ChannelCountryScraper(object):
 	def __init__(self):
 		self.channel_about_urls = []
 		self.channel_countries = []
+		self.channel_subscribers = []
 
 
 	def run(self):
 		self.read_youtube_urls()
 		self.get_page_source()
-		self.channel_country_write()
+		self.channel_country_additional()
+		self.channel_subscriber_additional()
 		self.driver.close()
 
 
@@ -64,12 +66,32 @@ class ChannelCountryScraper(object):
 	def parse_channel_country(self):
 		soup = BeautifulSoup(self.current_html, 'html.parser')
 		'''
-			CuntryOfIntExtractionFunction
+		channelSubscriberOfIntExtractionFunction
 		'''
+		
+		channel_subscriber_i = soup.find("yt-formatted-string", class_="style-scope ytd-c4-tabbed-header-renderer")
+		channel_subscriber_lstrip = str(channel_subscriber_i).lstrip('<yt-formatted-string class="style-scope ytd-c4-tabbed-header-renderer" id="subscriber-count">')
+		channel_subscriber_rstrip = channel_subscriber_lstrip.rstrip('</yt-formatted-string>')
+		if "万" in channel_subscriber_rstrip:
+			channel_subscriber_replace = channel_subscriber_rstrip.replace(' ', '')
+			channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
+			channel_subscriber_add_million = channel_subscriber_sub + '00'
+			channel_subscriber_material = int(channel_subscriber_add_million)
+		else:
+			channel_subscriber_replace = channel_subscriber_rstrip.replace(' ', '')
+			channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
+			channel_subscriber_material = int(channel_subscriber_sub)
+			'''
+			CuntryOfIntExtractionFunction
+			'''
 		for i in soup.find_all("td", class_="style-scope ytd-channel-about-metadata-renderer"):
 			cuntry_i_findall = re.findall('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">.*</yt-formatted-string>', str(i))
 			cuntry_i_replace = str(cuntry_i_findall).replace('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">', '').replace('</yt-formatted-string>', '')
 			cuntry = str(cuntry_i_replace).replace("['", '').replace("']", '')
+			'''
+			channelSubscriberOfIntExtractionFunction
+			'''
+			channel_subscriber = channel_subscriber_material
 			'''
 			NoneExclusion
 			'''
@@ -81,14 +103,21 @@ class ChannelCountryScraper(object):
 				continue
 			if str(cuntry):
 				self.channel_countries.append(cuntry)
+				self.channel_subscribers.append(channel_subscriber)
 
 
-	def channel_country_write(self):
+	def channel_country_additional(self):
 		df = pd.read_csv('sample.csv')
-		print(df)
 		df['channel_country'] = self.channel_countries
-		print(df)
 		pd.DataFrame(df).to_csv('sample.csv',index=False)
+
+
+	def channel_subscriber_additional(self):
+		print(self.channel_subscribers)
+		df = pd.read_csv('sample.csv')
+		df['channel_subscriber'] = self.channel_subscribers
+		pd.DataFrame(df).to_csv('sample.csv',index=False)
+		print(df)
 
 
 if __name__ == "__main__":
