e205b476de1ca93998b87989b2d2e4d336bdadc9
“maki5588” <your-mail@example.com>
2020-08-03 18:26:40 +0900

    fix

--- youtube_extract.py
+++ youtube_extract.py
@@ -25,8 +25,6 @@ class ChannelCountryScraper(object):
 		self.get_page_source()
 		self.channel_country_write()
 		self.driver.close()
-		# self.parse_channel_country()
-		# self.channel_country_data_save_as_csv_file()
 
 
 	def read_youtube_urls(self):
@@ -55,7 +53,6 @@ class ChannelCountryScraper(object):
 				for j in range(100):
 					actions.send_keys(Keys.PAGE_DOWN)
 				actions.perform()
-				# sleep(1)
 				html = self.driver.page_source
 				if self.current_html != html:
 					self.current_html=html
@@ -84,59 +81,14 @@ class ChannelCountryScraper(object):
 				continue
 			if str(cuntry):
 				self.channel_countries.append(cuntry)
-				# print(cuntry)
-				# self.channel_country_write()
 
 
-# import StringIO
-# import io 
-# import urllib.request
-# from PIL import Image
 	def channel_country_write(self):
-		# # channel_url_data = pd.read_csv('sample.csv',index_col='channel_url')
-		# # print(df)
-		# # print(self.channel_url)
-		# # print(self.channel_countries)
-		# # data['new_column'] = ''
-		# # s = pd.Series(self.channel_countries, self.channel_url)
-		# print(self.channel_countries)
-		# # print(self.channel_url)
 		df = pd.read_csv('sample.csv')
-		# s = pd.Series({self.channel_countries: self.channel_url}, name='channel_country')
-		# # s3 = s3.append(s4)
-		# # df['channel_cuntry'] = s
-		# # print(assign(s.values))
-		# # print(s)
-		# df.insert(len(df.columns), 'channel_country',0)
-		# # pd.DataFrame(df_x).to_csv('sample.csv',index=True)
-		# # print(channel_country_save)
-		# print(df)
 		print(df)
-		# df['new_column'] = 'value'
 		df['channel_country'] = self.channel_countries
 		print(df)
 		pd.DataFrame(df).to_csv('sample.csv',index=False)
-		# data = {
-		# # "title": self.titles,
-		# # "video_url": self.video_urls,
-		# # "view": self.views,
-		# # "channel_url": self.channel_urls,
-		# # "channel_name": self.channel_names,
-		# # "video_time": self.video_times,
-		# "channel_country": self.channel_countries
-		# }
-		# print(self.channel_countries)
-  #       # print(self.channel_countries)
-		# pd.DataFrame(data).to_csv('sample.csv',index=True)
-		# for index, row in data.iterrows():
-		# 	data['new_column'][index] = self.channel_countries
-		# 	data.to_csv('sample.csv', index=False)
-		# 	print(open('sample.csv').read())
-# s = io.BytesIO(text)
-# with open('sample.csv', 'w') as f:
-# 	for line in s:
-# 		f.write(line)
-
 
 
 if __name__ == "__main__":
